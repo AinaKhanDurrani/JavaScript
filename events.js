4721eff9e34e20302db2e067939cdac51a93a839
AinaKhanDurrani <ainakhan464@gmail.com>
2023-12-16 14:51:15 +0500

    Events

--- events.js
+++ events.js
@@ -1,3 +1,12 @@
 function greetings(){
     alert("Welcome to the world of java Script")
+    document.write("Welcome to my world")
+}
+
+function greet_user(username){
+    document.write(`Welcome to Home: ${username}`)
+}
+
+function greet(username){
+    document.write(`Welcome to Home: ${username}`)
 }
